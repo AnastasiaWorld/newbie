eece555f6ee82de1dc0902d0e50d296cd0714594
BabiWorld <112662733+BabiWorld@users.noreply.github.com>
2022-09-04 16:26:50 +0200

    Add files via upload

---
+++ newbie.js
@@ -0,0 +1,6 @@
+function revealMenu(){
+    document.getElementById("megamenu1").style.display="inline-grid";
+}
+function hideMenu(){
+    document.getElementById("megamenu1").style.display="none";
+}
